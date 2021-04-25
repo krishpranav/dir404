a5967f56a7ce58a0b68a52c2d5a4d576067c29c4
krishpranav <krisna.pranav@gmail.com>
2021-04-25 19:41:39 +0530

    added colors

--- main.go
+++ main.go
@@ -15,3 +15,20 @@ import (
 	"sync"
 	"time"
 )
+
+// setted as false to run the tool for running till getting the response
+var SingleScan = false
+
+// colors
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var Blue = "\033[34m"
+var Purple = "\033[35m"
+var Cyan = "\033[36m"
+var Gray = "\033[37m"
+var White = "\033[97m"
+var Dark = "\033[90m"
+var clear map[string]func()
+

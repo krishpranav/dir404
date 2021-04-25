24115065a3a4a6522ae7945e31b69a1a43184be2
krishpranav <krisna.pranav@gmail.com>
2021-04-25 19:44:02 +0530

    function for initializing linux and windows commands

--- main.go
+++ main.go
@@ -32,3 +32,29 @@ var White = "\033[97m"
 var Dark = "\033[90m"
 var clear map[string]func()
 
+// init function
+func init() {
+	clear = make(map[string]func()) // intialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") // linux example
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") // windows example
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	if runtime.GOOS == "windows" {
+		Reset = ""
+		Dark = ""
+		Red = ""
+		Green = ""
+		Yellow = ""
+		Blue = ""
+		Purple = ""
+		Cyan = ""
+		Gray = ""
+		White = ""
+	}
+}
